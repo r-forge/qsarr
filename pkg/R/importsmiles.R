1eb1591093557aa32e09463a07e84f632850c658
rcbraga (Dr. ) <rcbraga@gmail.com>
2012-11-22 12:04:07 +0000

    Old
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qsarr@7 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/importsmiles.R
+++
@@ -1,6 +0,0 @@
-
-###########Generating 3D CDK descriptors###########
-# importar dados no formato sdf na sequência: 1. y resposta biológica 2. x - descritores
-qsar.activity <- read.csv(file.choose(),sep = ",", header=TRUE)
-molecules <- load.molecules(file.choose())
-qsar.descriptors <- eval.desc(molecules, cdk.2d)
