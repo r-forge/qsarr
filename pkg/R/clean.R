87c7e3810e48729cf076b80e2ed57b3c47b2df17
rcbraga (Dr. ) <rcbraga@gmail.com>
2012-11-23 11:25:54 +0000

    ok
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qsarr@17 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/clean.R
+++ pkg/R/clean.R
@@ -1,16 +1,23 @@
 clean <- function(data = qsar.descriptors, ... ){
   qsar.descriptors.na <- subset(data, select=colMeans(is.na(qsar.descriptors)) == 0)
   assign("qsar.descriptors.na", qsar.descriptors.na, envir=.GlobalEnv)
-  nzv <- nearZeroVar(qsar.descriptors.na) 
+  nzv.func <- nearZeroVar(qsar.descriptors.na)
+  assign("nzv", nzv.func, envir=.GlobalEnv)
   if (length(nzv > 1) != 0)  { 
     # Remove Zero variance
-    nzv <- nearZeroVar(qsar.descriptors.na)
-    qsar.descriptors.nzv <- qsar.descriptors.na[, -nzv]
+    nzv.func <- nearZeroVar(qsar.descriptors.na)
+    assign("nzv.func", nzv.func, envir=.GlobalEnv)
+    qsar.descriptors.nzv.func <- qsar.descriptors.na[, -nzv]
+    assign("qsar.descriptors.nzv", qsar.descriptors.nzv.func, envir=.GlobalEnv)
     # Remover correlação acima 90%
-    descriptors.correlation <- cor(qsar.descriptors.nzv)
-    descriptors.correlation.9 <- findCorrelation(descriptors.correlation, cutoff = .9)
-    qsar.descriptors.cor <- qsar.descriptors.nzv[,-descriptors.correlation.9]
+    descriptors.correlation.func <- cor(qsar.descriptors.nzv)
+    assign("descriptors.correlation", descriptors.correlation.func, envir=.GlobalEnv)
+    descriptors.correlation.9.func <- findCorrelation(descriptors.correlation, cutoff = .9)
+    assign("descriptors.correlation.9", descriptors.correlation.9.func, envir=.GlobalEnv)
+    qsar.descriptors.cor.func <- qsar.descriptors.nzv[,-descriptors.correlation.9]
+    assign("qsar.descriptors.cor", qsar.descriptors.cor.func, envir=.GlobalEnv)
   } else { 
-    qsar.descriptors.cor <- qsar.descriptors.na
+    qsar.descriptors.cor.func <- qsar.descriptors.na
+    assign("qsar.descriptors.cor", qsar.descriptors.cor.func, envir=.GlobalEnv)
   } 
 }
