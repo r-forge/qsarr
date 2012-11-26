ae69ef2689596e5e690be8c7a7b47105841a0e90
rcbraga (Dr. ) <rcbraga@gmail.com>
2012-11-26 12:56:19 +0000

    ok
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qsarr@37 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/model.R
+++ pkg/R/model.R
@@ -9,7 +9,7 @@ cv <- 1
 models <- function(cores=2,type=small,method=cv,number = 10,repeats = 3, ...){
 {if (((type) == small & (method) == cv)) {
 #######################################################
-#############Calculating MODELS STANDARD ##############
+#############Calculating MODELS SMALL ##############
 #######################################################
 #######################################################
 ############Paralelization number of cores#############
@@ -27,7 +27,7 @@ fitControl <- caret::trainControl(## 10-fold CV
     ## Save all the resampling results
     returnResamp = "all")
 #######################################################
-###########Calculating Metric for PLS model############
+#################Calculating  PLS model################
 #######################################################
 plsFit<- caret::train(Train.descriptors, unlist(Train.activity), 
                 "pls",
@@ -41,6 +41,56 @@ predVals.pls <- caret::extractPrediction(list(plsFit),
   png(file="models-pls.png", width=1430, height=1004, res=144)
   plot(caret::plotObsVsPred(predVals.pls))
   dev.off()
+#######################################################
+################Calculating  MARS model################
+#######################################################
+earthFit <- caret::train(Train.descriptors, unlist(Train.activity),
+                  "earth",
+                  tuneLength = 20,
+                  trControl = fitControl)
+.GlobalEnv[["earthFit"]] <- earthFit
+predVals.earth <- caret::extractPrediction(list(earthFit),
+                                    testX = Test.descriptors,
+                                    testY = Test.activity)
+.GlobalEnv[["predVals.earth"]] <- predVals.earth
+png(file="models-earth.png", width=1430, height=1004, res=144)
+plot(caret::plotObsVsPred(predVals.earth))
+dev.off()
+#######################################################
+################Calculating  KNN model#################
+#######################################################
+knnFit <- care::train(Train.descriptors, unlist(Train.activity),
+                "knn",
+                tuneLength = 20,
+                trControl = fitControl)
+.GlobalEnv[["knnFit"]] <- knnFit
+predVals.knn <- caret::extractPrediction(list(knnFit),
+                                  testX = Test.descriptors,
+                                  testY = Test.activity)
+.GlobalEnv[["predVals.knn"]] <- predVals.knn
+png(file="models-knn.png", width=1430, height=1004, res=144)
+plot(caret::plotObsVsPred(predVals.knn))
+dev.off()
+#######################################################
+##########Calculating random forest model#############
+#######################################################
+rfFit <- train(Train.descriptors, unlist(Train.activity),
+               "rf",
+               tuneLength = 20,
+               trControl = fitControl)
+.GlobalEnv[["rfFit"]] <- rfFit
+predVals.rf <- extractPrediction(list(rfFit),
+                                 testX = Test.descriptors,
+                                 testY = Test.activity)
+png(file="models-random_forest.png", width=1430, height=1004, res=144)
+plot(caret::plotObsVsPred(predVals.rf))
+dev.off()
+#######################################################
+#############Calculating MODELS STANDARD ##############
+#######################################################
+#######################################################
+############Paralelization number of cores#############
+#######################################################  
 } else if (((type) == standard & (method) == cv)){;
   library(doMC)
   doMC::registerDoMC(cores)
@@ -57,17 +107,23 @@ predVals.pls <- caret::extractPrediction(list(plsFit),
   ###########Calculating Metric for PLS model############
   #######################################################
   #################################
-  earthFit <- train(Train.descriptors, unlist(Train.activity),
+earthFit <- train(Train.descriptors, unlist(Train.activity),
                     "earth",
                     tuneLength = 20,
                     trControl = fitControl)
-  predVals.earth <- extractPrediction(list(earthFit),
+predVals.earth <- extractPrediction(list(earthFit),
                                       testX = Test.descriptors,
                                       testY = Test.activity)
-  png(file="models-earth.png", width=1430, height=1004, res=144)
-  plot(plotObsVsPred(predVals.earth))
-  dev.off()
-} else if (((type) == standard & (method) == cv)){;
+png(file="models-earth.png", width=1430, height=1004, res=144)
+plot(plotObsVsPred(predVals.earth))
+dev.off()
+#######################################################
+#############Calculating MODELS STANDARD ##############
+#######################################################
+#######################################################
+############Paralelization number of cores#############
+#######################################################  
+} else if (((type) == extended & (method) == cv)){;
 library(doMC)
 doMC::registerDoMC(cores)
                                                   
