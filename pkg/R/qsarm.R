3162124554a10d327835e5fddf85ecbd606bd3f6
rcbraga (Dr. ) <rcbraga@gmail.com>
2012-12-03 12:28:09 +0000

    ok
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qsarr@49 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/qsarm.R
+++ pkg/R/qsarm.R
@@ -5,8 +5,8 @@ r2 <- function(y, equation, ... ){
   1 - (sum((y-predict(equation))^2)/sum((y-mean(y))^2))
 }
 #####
-r2pred <- function(y, x, ... ){
-  1-(sum((y-x)^2))/(sum(((y-mean(y))^2)))
+r2pred <- function(ypred.test, yobs.test, yobs.train, ... ){
+  1-(sum((ypred.test-yobs.test)^2))/(sum( ((yobs.test-mean(yobs.train))^2) ))
 }
 #######################################################
 ###########Loading all rm2 Metric functions############
@@ -36,6 +36,27 @@ delta.rm2 <- function(y, x, ... ){
   }
 }
 
-Q2f3 <- function(y,x, ytrain,nex, ntrain, ... ){
-  1- ((sum(y-x)^2)/nex) / ((sum(((y-mean(ytrain))^2)))/ntrain)
+
+Q2f1 <- function(ypred.test, yobs.test, yobs.train, ... ){
+  1-(sum((ypred.test-yobs.test)^2))/(sum( ((yobs.test-mean(yobs.train))^2) ))
+}
+
+Q2f2 <- function(ypred.test, yobs.test, yobs.train, ... ){
+  1-(sum((ypred.test-yobs.test)^2))/(sum( ((yobs.test-mean(yobs.test))^2) ))
+}
+
+Q2f3 <- function(ypred.test, yobs.test,yobs.train, nex, ntrain, ... ){
+  1 - ((sum((ypred.test-yobs.test)^2))/nex) / ((sum((yobs.train-mean(yobs.train))^2) )/ntrain)
+}
+
+CCC <- function(ypred.test, yobs.test, nex, ... ){
+  (2*(sum((yobs.test-mean(yobs.test))*(ypred.test-mean(ypred.test)))))/( (sum(((yobs.test-mean(yobs.test))^2))) + (sum(((ypred.test-mean(ypred.test))^2))) +  (nex*(mean(yobs.test)-mean(ypred.test)^2))  )
+}
+
+RMSEP <- function(ypred.test, nex, ... ){
+  (sum((ypred.test-mean(ypred.test))^2)) / nex
+}
+
+RMSEC <- function(ypred.train, ntrain, ... ){
+  (sum((ypred.train-mean(ypred.train))^2)) / ntrain
 }
