693fa33438fe5962068f31fd6b80efe702c05e96
rcbraga (Dr. ) <rcbraga@gmail.com>
2013-01-21 14:58:49 +0000

    ok
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qsarr@55 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/qsarr.R
+++ pkg/R/qsarr.R
@@ -2,13 +2,13 @@
 #carrega bibliotecas necessárias
 
 qsar.workflow <- function(cores=2,qsar=2,activ=NULL,descrip=NULL,split=random,prop=0.75,type=small,method=cv,number=10,repeats=3,tuneLength=15, ... ){
- # library(doMC)
-  #cdk()
-  #import(qsar,activ, descrip)
-  #clean()
-  #split(split, prop, number,repeats,tuneLength)
-  #models(cores,type,method,number,repeats,tuneLength)
-  metrics(type)
+ library(doMC)
+cdk()
+import(qsar,activ, descrip)
+clean()
+split(split, prop, number,repeats,tuneLength)
+models(cores,type,method,number,repeats,tuneLength)
+metrics(type)
 }
 
 
---
+++ pkg/R/y.R
@@ -0,0 +1,51 @@
+random.y <- function(A) { 
+  for (i in 1:nrow(A)) { A[,1] <- sample(A[,1]) } 
+  outplut=list(A)
+} 
+
+yrandomization <- function(A=qsar.activity,matrix.x=qsar.descriptors.cor, repl=10) { 
+{
+  QY <- train(matrix.x, unlist(A),
+              "pls",
+              tuneLength = 20,
+              trControl = trainControl(
+                method = "boot632", returnResamp = "all"))
+  .GlobalEnv[["QY"]] <- QY
+  result.QY<- QY$results[which.min(QY$results[,2] ), ]
+  .GlobalEnv[["result.QY"]] <- result.QY
+  QY.q2 <- result.QY$Rsquared
+  .GlobalEnv[["QY.q2"]] <- QY.q2
+}
+
+{  
+  random.A <- replicate(repl, random.y(A))
+  random.mat.dataframe = data.frame(random.A)
+  .GlobalEnv[["random.mat.dataframe"]] <- random.mat.dataframe
+}
+
+
+Q <- data.frame(sapply( 1:ncol( random.mat.dataframe ), 
+                        function( i ) 
+                          train(matrix.x, unlist(random.mat.dataframe[,i]),
+                                "pls",
+                                tuneLength = 20,
+                                trControl = trainControl(
+                                  method = "boot632", returnResamp = "all"))
+                        
+))
+.GlobalEnv[["Q"]] <- Q
+
+extract <- data.frame(sapply( 1:ncol( Q ), 
+                              function( i ) 
+                                Q[,i]$results[which.min(Q[,i]$results[,2] ), ]
+                              
+))
+.GlobalEnv[["extract"]] <- extract
+
+extract.q <- data.frame(sapply( 1:ncol( extract ), 
+                                function( i ) 
+                                  extract[,i]$Rsquared
+                                
+))
+.GlobalEnv[["extract.q"]] <- extract.q
+}
